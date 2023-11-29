bc2b3e1a0a2cf005ff85c952f37d86f78b897169
perthDDD <140643274+perthDDD@users.noreply.github.com>
2023-11-30 00:48:03 +0800

    Add files via upload

---
+++ login-page.js
@@ -0,0 +1,16 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "rn3o2infoin3in" && password === "vnwoifn32oin351") {
+        alert("You have successfully logged in.");
+        window.open("https://funky-wave-252.notion.site/BTP6-ee6a9fb79ce34f018267a70a35d76838?pvs=4");
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
