541766327be9cd4228569243f72c503fed8fae43
perthDDD <140643274+perthDDD@users.noreply.github.com>
2023-11-30 00:49:04 +0800

    update login username, password, and notion hyperlink

--- login-page.js
+++ login-page.js
@@ -7,9 +7,9 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "rn3o2infoin3in" && password === "vnwoifn32oin351") {
+    if (username === "XsGBVzVfCS" && password === "rukF53zw7Q") {
         alert("You have successfully logged in.");
-        window.open("https://funky-wave-252.notion.site/BTP6-ee6a9fb79ce34f018267a70a35d76838?pvs=4");
+        window.open("https://funky-wave-252.notion.site/BTP7-8435a3ea2c654214a905d3dbd9684c85?pvs=4");
     } else {
         loginErrorMsg.style.opacity = 1;
     }
